cb8b21b0ce3e6948be5ee91a1ba2f4bb2089edbc
Wild Fi1 <wildfi1@MacBook-Pro-de-WildFi.local>
2025-07-22 14:36:02 -0300

    img

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-fetch("https://script.google.com/macros/s/AKfycbzYqBCZ-IJ336PKORcp9IN7UAjowGRXCf7N_fy51zytPl3yy0X17ZqQx0fb36QMD_rRaw/exec") // tu URL pública de Web App
-    .catch(err => console.error("Error al registrar visita:", err)); F
+setTimeout(function () {
+    fetch("https://script.google.com/macros/s/AKfycbzYqBCZ-IJ336PKORcp9IN7UAjowGRXCf7N_fy51zytPl3yy0X17ZqQx0fb36QMD_rRaw/exec")
+        .catch(err => console.error("Error al registrar visita:", err));
+
+    window.location.href = "https://www.instagram.com/mogul.uy/#";
+}, 3000);
